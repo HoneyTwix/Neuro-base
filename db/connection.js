b7699a59d5496908eb370d4e81c8224385497ef1
HoneyTwix <chocolateenchanted@gmail.com>
2022-10-26 16:47:49 -0500

    data

--- db/connection.js
+++ db/connection.js
@@ -10,10 +10,10 @@ const mysql = require('mysql2');
 // });
 
 const db = mysql.createConnection({ 
-HOST: "us-cdbr-east-06.cleardb.net",
-USER: "b1fb543e385434",
-PASSWORD: "77801be9",
-DB: "heroku_1550511dffaec6e"
+host: "us-cdbr-east-06.cleardb.net",
+user: "b1fb543e385434",
+password: "77801be9",
+database: "heroku_1550511dffaec6e"
 });
 
 module.exports = db;
