dca363420864f61b7a585cd85eff59c2ba0f38a5
HoneyTwix <chocolateenchanted@gmail.com>
2022-10-25 22:04:01 -0500

    connector

--- db/connection.js
+++ db/connection.js
@@ -9,11 +9,11 @@ const mysql = require('mysql2');
 //   database: 'neuro'
 // });
 
-const db ={ 
+const db = mysql.createConnection({ 
 HOST: "us-cdbr-iron-east-02.cleardb.net",
 USER: "b7e2437887xxxa",
 PASSWORD: "0200xxx6",
 DB: "heroku_7643ec736354xxx"
-};
+});
 
 module.exports = db;
