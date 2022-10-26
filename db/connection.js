cda3cea51c87b843624fd3016e71b2f11c93e052
HoneyTwix <chocolateenchanted@gmail.com>
2022-10-25 22:00:08 -0500

    connect

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,19 @@
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
-  host: 'localhost',
-  // Your MySQL username,
-  user: 'root',
-  // Your MySQL password
-  password: 'honeytwix1801',
-  database: 'neuro'
-});
+// const db = mysql.createConnection({
+//   host: 'localhost',
+//   // Your MySQL username,
+//   user: 'root',
+//   // Your MySQL password
+//   password: 'honeytwix1801',
+//   database: 'neuro'
+// });
+
+const db ={ 
+HOST: "us-cdbr-iron-east-02.cleardb.net",
+USER: "b7e2437887xxxa",
+PASSWORD: "0200xxx6",
+DB: "heroku_7643ec736354xxx"
+};
 
 module.exports = db;
